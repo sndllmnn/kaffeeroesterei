7015a9c8bd97361143cff1bc0801fa6bce1e1d24
Sina <user@MacBook-Air-de-User.local>
2022-02-20 16:36:50 +0100

    cafe subpage gallery, flex 2 columns mobile only

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,12 @@ myFunction();
 const result = multiply(2, 5);
 
 console.log(result); */
+
+import Masonry from 'masonry-layout';
+
+window.onload = () => {
+    const grid = document.querySelector(".cafe__grid");
+    const masonry = new Masonry(grid, {
+        itemSelector: '.grid__item',
+    });
+};
