45a3a9512dc96b0655af0f8033800c673fedca6d
Sina <user@MacBook-Air-de-User.local>
2022-02-22 18:00:24 +0100

    toggle menu bar mobile

---
+++ src/js/toggleMenu.js
@@ -0,0 +1,15 @@
+const burgerMenu = document.querySelector(".burgerMenu");
+const navSidebar = document.querySelector(".toggleMenu");
+const closeButton = document.querySelector(".closeButton");
+
+function openBurgerMenu() {
+    navSidebar.classList.toggle('show');
+    
+};
+
+function closeWindow() {
+    navSidebar.classList.remove('show');
+}
+
+burgerMenu.addEventListener('click', openBurgerMenu);
+closeButton.addEventListener('click', closeWindow);
