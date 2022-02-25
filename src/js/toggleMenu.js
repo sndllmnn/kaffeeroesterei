69f941b56f4163c1a119e84d80b564f818319691
Sina <user@MacBook-Air-de-User.local>
2022-02-25 15:43:10 +0100

    toggle menu scrollstop when open

--- src/js/toggleMenu.js
+++ src/js/toggleMenu.js
@@ -1,13 +1,17 @@
 const burgerMenu = document.querySelector(".burgerMenu");
 const navSidebar = document.querySelector(".toggleMenu");
 const closeButton = document.querySelector(".closeButton");
+const body = document.querySelector("body");
 
 function openBurgerMenu() {
     navSidebar.classList.toggle('show'); 
+    body.classList.toggle('scrollStop');
 };
 
 function closeWindow() {
     navSidebar.classList.remove('show');
+    body.classList.remove('scrollStop');
+
 }
 
 burgerMenu.addEventListener('click', openBurgerMenu);
