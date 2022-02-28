8da70d71ef6a43e40910bbb8ec9db2dbfc41439e
Sina <user@MacBook-Air-de-User.local>
2022-02-28 20:00:57 +0100

    shop.js and shop.html

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 
-import shop from "./shop";
     
 
 
--- src/js/shop.js
+++ src/js/shop.js
@@ -1,45 +1,57 @@
-// pathname shop: /subpages/shop.html
+/* pathname shop: /subpages/shop.html */
 
-// const currentPath = window.location.pathname; -> wenn aktiv geht's nicht,
-// wird aber auch so erkannt
+/* const currentPath = window.location.pathname; -> wenn aktiv geht's nicht,
+wird aber auch so erkannt */
 
-// import products from '../../products.json'; -> wie?
+import products from '../../products.json'; 
 
-function shop() {
-    if (currentPath = "/subpages/shop.html") {
-        const productSection = window.document.querySelector('.products');
-        /* const productContainer = window.document.createElement('div'); */
-        const product = window.document.createElement('div');
+function createProductElements() {
+    const productSection = document.querySelector('.products');
 
+    /* if (currentPath = "/subpages/shop.html") { */
+       
 
-        // weiß nicht mehr ,was hier product ist, ergibt keinen Sinn, nicht festgelegt:
+        products.forEach((product) => { // für jedes Array Element von der products.json
 
-        products.forEach(product => { // für jedes Array Element von der products.json
+        const productElement = document.createElement('div'); 
+        productElement.classList.add('product');
 
-            const productTitle = document.createElement('p');
-            productTitle.classList.add('productTitle');
+        const productImageWrapper = document.createElement('div');
+        productImageWrapper.classList.add('productImageWrapper');
 
+        /*const productImage = document.createElement('img');
+        productImage.classList.add('productImage');
+        const productImageFile = document.createElement(product.productImage);
+        productImage.appendChild(productImage); */
+    
+        const productTitle = document.createElement('p');
+        productTitle.classList.add('productTitle');
+        const productTitleText = document.createTextNode(product.productName);
+        productTitle.appendChild(productTitleText);
 
-            const productPrice = document.createElement('p');
-            productPrice.classList.add('productPrice');
+        const productPrice = document.createElement('p');
+        productPrice.classList.add('productPrice');
+        const productPriceText = document.createTextNode(product.price);
+        productPrice.appendChild(productPriceText);
+        
 
+        /* const productDescription = document.createElement('p');
+        productDescription.classList.add('productDescription'); */
 
-            const productDescription = document.createElement('p');
-            productDescription.classList.add('productDescription');
-
-            const productImage = document.createElement('img');
-            productImage.classList.add('productImage');
-
-            const productIcons = document.createElement('div');
-            productImage.classList.add('productIcons');
+        /* const productIcons = document.createElement('div');
+        productImage.classList.add('productIcons');
 
         productSection.appendChild(product);
-        product.appendChild(productTitle, productPrice, productDescription, productImage, productIcons);
-        }
-    }
+        /* product.appendChild(productTitle, productPrice); */
+
+        [productImageWrapper/*, productImage*/, productTitle, productPrice].forEach((element) => {
+            productElement.appendChild(element);
+        });
+        productSection.appendChild(productElement);
+
+    });
 }
 
-window.addEventListener("DOMContentLoaded", shop);
+window.addEventListener("DOMContentLoaded",createProductElements);
 
-/* named export
-export {shop}; */
+/* named export export {shop}; */
