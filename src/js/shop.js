112ffdf278dae62ebbd72d1e353239310b6aabb0
Sina <user@MacBook-Air-de-User.local>
2022-03-01 16:12:40 +0100

    product export 8 products in shop html via shop.js

--- src/js/shop.js
+++ src/js/shop.js
@@ -3,55 +3,57 @@
 /* const currentPath = window.location.pathname; -> wenn aktiv geht's nicht,
 wird aber auch so erkannt */
 
-import products from '../../products.json'; 
+import products from "../../products.json";
 
 function createProductElements() {
-    const productSection = document.querySelector('.products');
+  const productSection = document.querySelector(".products");
 
-    /* if (currentPath = "/subpages/shop.html") { */
-       
+  /* if (currentPath = "/subpages/shop.html") { */
 
-        products.forEach((product) => { // für jedes Array Element von der products.json
+  products.forEach((product) => {
+    // für jedes Array Element von der products.json
 
-        const productElement = document.createElement('div'); 
-        productElement.classList.add('product');
+    const productElement = document.createElement("div");
+    productElement.classList.add("product");
 
-        const productImageWrapper = document.createElement('div');
-        productImageWrapper.classList.add('productImageWrapper');
+    const productImageWrapper = document.createElement("div");
+    productImageWrapper.classList.add("productImageWrapper");
 
-        /*const productImage = document.createElement('img');
-        productImage.classList.add('productImage');
-        const productImageFile = document.createElement(product.productImage);
-        productImage.appendChild(productImage); */
-    
-        const productTitle = document.createElement('p');
-        productTitle.classList.add('productTitle');
-        const productTitleText = document.createTextNode(product.productName);
-        productTitle.appendChild(productTitleText);
+    const productImage = document.createElement("img");
+    productImage.classList.add('productImage--shop'),
+    productImage.src = "/images/kaffeepackungCostaRica.png";
 
-        const productPrice = document.createElement('p');
-        productPrice.classList.add('productPrice');
-        const productPriceText = document.createTextNode(product.price);
-        productPrice.appendChild(productPriceText);
-        
+    const productTitle = document.createElement("p");
+    productTitle.classList.add("productTitle");
+    const productTitleText = document.createTextNode(product.productName);
 
-        /* const productDescription = document.createElement('p');
+    const productPrice = document.createElement("p");
+    productPrice.classList.add("productPrice");
+    const productPriceText = document.createTextNode(product.price);
+
+    const productIcons = document.createElement("div");
+    productIcons.src = "/components/icons.hbs";
+
+    /* const productDescription = document.createElement('p');
         productDescription.classList.add('productDescription'); */
 
-        /* const productIcons = document.createElement('div');
+    /* const productIcons = document.createElement('div');
         productImage.classList.add('productIcons');
 
         productSection.appendChild(product);
         /* product.appendChild(productTitle, productPrice); */
 
-        [productImageWrapper/*, productImage*/, productTitle, productPrice].forEach((element) => {
-            productElement.appendChild(element);
-        });
-        productSection.appendChild(productElement);
-
+    [productImageWrapper, productTitle, productPrice].forEach((element) => {
+      productElement.appendChild(element);
     });
+    productSection.appendChild(productElement);
+    productImageWrapper.appendChild(productImage);
+    productTitle.appendChild(productTitleText);
+    productPrice.appendChild(productPriceText);
+    productElement.appendChild(productIcons);
+  });
 }
 
-window.addEventListener("DOMContentLoaded",createProductElements);
+window.addEventListener("DOMContentLoaded", createProductElements);
 
 /* named export export {shop}; */
