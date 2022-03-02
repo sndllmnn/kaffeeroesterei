b4ab092bfdac4bdbdb685ab2ae2f97bbf309ff71
Sina <user@MacBook-Air-de-User.local>
2022-03-02 20:00:53 +0100

    nav try

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,23 @@
 function changeNavColor() {
     const navBar = document.querySelector('nav');
-    navBar.classList.add('.sticky');
+    navBar.classList.toggle("sticky", window.scrollY > 0);
+
+    /* navBar.classList.add('.sticky'); */
 }
 
-document.addEventListener('scroll', changeNavColor());
+document.addEventListener('scroll', changeNavColor);
+
+
         
+/* function (){
+  var header = document.querySelector(".primary-header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+}) */
+
+
+
+
+
 
 
 /* funktioniert nicht, grid gallery cafe subpage 
